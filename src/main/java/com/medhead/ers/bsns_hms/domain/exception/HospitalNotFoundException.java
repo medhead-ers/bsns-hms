1911f0d3111f3eb129e3c319ef917b7647be4946
David EVAN <devan.exa@gmail.com>
2023-03-09 07:42:49 +0100

    Fix : Code smell

--- src/main/java/com/medhead/ers/bsns_hms/domain/exception/HospitalNotFoundException.java
+++ src/main/java/com/medhead/ers/bsns_hms/domain/exception/HospitalNotFoundException.java
@@ -1,7 +1,5 @@
 package com.medhead.ers.bsns_hms.domain.exception;
 
-import java.util.UUID;
-
 public class HospitalNotFoundException extends  RuntimeException{
     public HospitalNotFoundException(String identifier) {
         super("Could not find hospital with identifier : " +identifier);
