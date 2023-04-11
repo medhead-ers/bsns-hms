00302caa2054ad5097051be3b87f970c12331e0a
David EVAN <devan.exa@gmail.com>
2023-04-11 08:03:08 +0200

    feature/cross-origin-policy
    WIP

--- src/main/java/com/medhead/ers/bsns_hms/application/controller/HospitalController.java
+++ src/main/java/com/medhead/ers/bsns_hms/application/controller/HospitalController.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class HospitalController {
     @Autowired
     private HospitalService hospitalService;
